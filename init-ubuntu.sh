9d107d924b874b108c1e5918187b07bdebd787fc
Glavo <zjx001202@126.com>
2020-07-09 00:15:16 +0800

    默认禁用 deb-src; 使用清华源安装 Docker

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -103,7 +103,7 @@ sudo dpkg -i ./netease-cloud-music.deb
 # install Docker (https://docs.docker.com/engine/install/ubuntu/)
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
